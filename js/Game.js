12da7b525745ee0fc3d3ce32b30f035f1205e149
PNWHiker <53063554+pnwhiker@users.noreply.github.com>
2021-01-14 20:07:24 -0800

    Initial workup on files

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+ class Phrase {
+    constructor (phrase) {
+         this.phrase = phrase;
+    }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Game {
+     constructor () {
+         this.missed = 0;
+         this.phrases = [
+             
+             {"saying A1"},
+             {"saying A2"},
+             {"saying A3"},
+             {"saying A4"},
+             {"saying A5"},
+             {"saying A6"},
+             {"saying A7"},
+             {"saying A8"},
+             {"saying A9"},
+             {"saying A10"},
+         ];
+         this.activePhrase = null;
+     }
+ }
